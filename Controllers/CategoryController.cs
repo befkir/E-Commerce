b7a0f2fa9a0ad2e35340c17aac5fb91efe867e7e
feshfaa <feshfaa@gmail.com>
2025-07-07 14:32:09 +0300

    Displayed all Categories

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using E_Commerce.Data;
+using E_Commerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objDiaryCategoryList = _db.Categories.ToList();
+            return View(objDiaryCategoryList);
+        }
+    }
+}
