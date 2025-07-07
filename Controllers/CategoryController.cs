3ca9c3dfa1a7b391f8693b554af867a3d0e561da
feshfaa <feshfaa@gmail.com>
2025-07-07 17:57:06 +0300

    Completed Category Edit

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -33,5 +33,32 @@ namespace E_Commerce.Controllers
             }
             return View(obj);
         }
+
+        
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id==0)
+            {
+                return NotFound();
+            }
+            var category = _db.Categories.Find(id);
+            if (category == null) {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
